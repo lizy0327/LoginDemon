df55e8ccb9e740af05f443526d4632b0792e9481
lizy0327 <lizy0327@gmail.com>
2015-07-28 10:06:03 +0800

    FirstSummary
    TheFirstSummary

---
+++ LoginClient/app/src/main/java/lzy/login/com/loginclient/service/ServiceRuleException.java
@@ -0,0 +1,12 @@
+package lzy.login.com.loginclient.service;
+
+/**
+ * Created by zhaoyang.li on 2015-07-18.
+ */
+public class ServiceRuleException extends Exception {
+
+    public ServiceRuleException(String message){
+        super(message);
+    }
+
+}
